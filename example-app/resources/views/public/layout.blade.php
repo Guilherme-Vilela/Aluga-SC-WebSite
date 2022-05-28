5631939c6537bb5986e77024f9f392024b32f100
Guilherme <guilherme@datamais.com.br>
2022-05-27 21:36:54 -0300

    feature: Adiconando telas principais

---
+++ example-app/resources/views/public/layout.blade.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<meta name="description" content="">
+<meta name="author" content="">
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+
+<title>Plotlist - Listing HTML5 Template</title>
+
+<!-- Bootstrap core CSS -->
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+
+<!-- Additional CSS Files -->
+<link rel="stylesheet" href="assets/css/fontawesome.css">
+<link rel="stylesheet" href="assets/css/templatemo-plot-listing.css">
+<link rel="stylesheet" href="assets/css/animated.css">
+<link rel="stylesheet" href="assets/css/owl.css">
+<!--
+
+-->
+</head>
+<body>
+
+@include('/public/header')
+@include('contact')
+@include('/public/footer')
+
+</body>
+</html>
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('../public/layout');
+});
+
+Route::get('/listing', function () {
+    return view('listing');
 });
